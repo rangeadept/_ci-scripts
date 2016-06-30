4fd5b1d19281abfda33f725d29d7449e7d4a8ec6
Range Strunk <range@adeptable.com>
2016-06-30 00:15:20 -0700

    more fixin

--- setup-gradle.sh
+++ setup-gradle.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+$ZIPFILE=$HOME/gradle.zip
+
 : ${GRADLE_VERSION:?"ERR: GRADLE_VERSION environment var must be set!"}
 
-curl -sLO https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-all.zip 
-unzip gradle-${GRADLE_VERSION}-all.zip
-ln -s gradle-${GRADLE_VERSION} $HOME/gradle
-rm gradle-${GRADLE_VERSION}-all.zip
+curl -o $ZIPFILE -sLO https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-all.zip 
+unzip $ZIPFILE $HOME/
+mv $HOME/gradle-${GRADLE_VERSION} $HOME/gradle
+rm $ZIPFILE
