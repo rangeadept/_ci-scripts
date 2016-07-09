7f11ccb5883166f5c39e0d265a5fd8500314e86f
Range Strunk <range@adeptable.com>
2016-07-08 19:44:17 -1000

    Adding cluster arg to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,19 @@
 set -e
 
 ns=$1
+cluster=$2
 
 if [ -z "${ns}" ]; then
+  echo "Namespace not specified"
   exit 1
 fi
+if [ -z "${cluster}" ]; then
+  echo "Cluster not specified"
+  exit 1
+fi
+
+# Get creds and set kubectl context for target cluster
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $cluster
 
 envsubst < kubernetes/deployment.yaml.tmpl > k8-deployment.yaml
 envsubst < kubernetes/service.yaml.tmpl > k8-service.yaml
